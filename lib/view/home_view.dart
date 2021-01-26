4bfe55a44fd1fa9a1db945d19c1eda3ea21fcb59
Adam <adam.gruber@cpnv.ch>
2021-01-26 20:33:34 +0100

    Home page stabe version (content is still uknown)

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -5,44 +5,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Currently on home page file"),
-      ),
       body: Container(
+        color: Colors.grey[700],
         child: Column(
           children: [
             Container(
               child: Text(
-                "Title",
-                style: TextStyle(fontSize: 25),
+                "TyperNerd",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
+                strutStyle: StrutStyle(fontSize: 40),
+              ),
+              alignment: Alignment.centerLeft,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(left: 25),
+              height: 100,
+            ),
+            Container(
+              child: Text(
+                "TyperNerd a été créé par deux développeurs pour pouvoir améliorer sa capacité en écriture sur mobile",
+                style: TextStyle(fontSize: 18, color: Colors.white),
               ),
               alignment: Alignment.center,
               width: MediaQuery.of(context).size.width,
               height: 100,
-              color: Colors.red,
+              margin: EdgeInsets.only(top: 45, left: 35, right: 35),
             ),
             Container(
-              child: Text(
-                "Content",
-                style: TextStyle(fontSize: 25),
-              ),
               alignment: Alignment.center,
               width: MediaQuery.of(context).size.width,
-              height: 250,
-              margin: EdgeInsets.only(top: 45),
-              color: Colors.yellow,
+              height: 200,
+              margin: EdgeInsets.only(top: 45, left: 35, right: 35),
+              color: Colors.white,
             ),
-            Container(
-              child: Text(
-                "Footer content",
-                style: TextStyle(fontSize: 25),
-              ),
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              height: 100,
-              margin: EdgeInsets.only(top: 45),
-              color: Colors.blue,
-            )
           ],
         ),
       ),
