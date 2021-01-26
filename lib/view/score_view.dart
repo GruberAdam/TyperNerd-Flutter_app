3f98587ca23f8cf93a81bdad85f6336f6aa41848
Adam <adam.gruber@cpnv.ch>
2021-01-26 20:14:24 +0100

    Score page stable version (history of games not finished)

--- lib/view/score_view.dart
+++ lib/view/score_view.dart
@@ -1,6 +1,6 @@
+import 'package:pluto_grid/pluto_grid.dart';
 import 'package:flutter/material.dart';
 import 'components/navigation-bar_component.dart';
-import 'package:horizontal_data_table/horizontal_data_table.dart';
 
 const SCORE_NAVBAR_INDEX = 2;
 
@@ -8,20 +8,23 @@ class ScorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[700],
       body: Container(
         child: Column(
           children: [
             Container(
               child: Text(
                 "TyperNerd",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
                 strutStyle: StrutStyle(fontSize: 40),
               ),
               alignment: Alignment.centerLeft,
               width: MediaQuery.of(context).size.width,
               padding: EdgeInsets.only(left: 25),
               height: 100,
-              color: Colors.red,
             ),
             Container(
               child: Column(
@@ -33,78 +36,67 @@ class ScorePage extends StatelessWidget {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("Username : <Smeilys> \n"),
-                              Text("Genre : <Male> \n"),
-                              Text("Record : <100 WPM> \n"),
-                              Text("Nb de tests : <50> \n"),
-                              Text("Joue depuis : <30.04.2020>")
+                              Text(
+                                "Username : <Smeilys> \n",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text("Genre : <Male> \n",
+                                  style: TextStyle(color: Colors.white)),
+                              Text("Record : <100 WPM> \n",
+                                  style: TextStyle(color: Colors.white)),
+                              Text("Nb de tests : <50> \n",
+                                  style: TextStyle(color: Colors.white)),
+                              Text("Joue depuis : <30.04.2020>",
+                                  style: TextStyle(color: Colors.white))
                             ],
                           ),
-                          color: Colors.pink,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white)),
                           height: 200,
-                          margin: EdgeInsets.only(left: 25),
+                          margin: EdgeInsets.only(left: 15),
                           padding:
                               EdgeInsets.only(left: 10, top: 10, right: 10),
                         ),
                         Container(
                           child: Text("WPM : <150>",
                               style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w500)),
-                          color: Colors.pink,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white)),
                           height: 50,
-                          margin: EdgeInsets.only(bottom: 150, left: 30),
+                          margin: EdgeInsets.only(bottom: 150, left: 10),
                           padding: EdgeInsets.only(right: 20, left: 10),
                           alignment: Alignment.centerLeft,
                         ),
                       ],
                     ),
-                    color: Colors.blue,
                     height: 250,
+                    margin: EdgeInsets.only(top: 30),
                   ),
                   Container(
-                    child: HorizontalDataTable(
-                      leftHandSideColumnWidth: 100,
-                      rightHandSideColumnWidth: 600,
-                      isFixedHeader: true,
-                      leftSideChildren: [],
-                      rightSideChildren: [],
-                      leftSideItemBuilder: _generateFirstColumnRow,
-                      headerWidgets: [Text("ID"), Text("WPM"), Text("Date")],
-                      itemCount: 3,
-                      rowSeparatorWidget: const Divider(
-                        color: Colors.black54,
-                        height: 1.0,
-                        thickness: 0.0,
-                      ),
+                    child: Text(
+                      "Historique de partie",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    color: Colors.purple,
                     width: MediaQuery.of(context).size.width,
-                    height: 100,
-                    margin: EdgeInsets.only(right: 25, left: 25, top: 10),
-                    padding: EdgeInsets.only(left: 10),
-                  )
+                    margin: EdgeInsets.only(left: 15, right: 15),
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.white)),
+                  ),
                 ],
               ),
-              alignment: Alignment.center,
               width: MediaQuery.of(context).size.width,
-              height: 400,
-              margin: EdgeInsets.only(top: 45),
-              color: Colors.yellow,
+              height: 475,
+              margin: EdgeInsets.only(top: 10, left: 15, right: 15),
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
             ),
           ],
         ),
       ),
       bottomNavigationBar: BottomNavBar(SCORE_NAVBAR_INDEX),
-    );
-  }
-
-  Widget _generateFirstColumnRow(BuildContext context, int index) {
-    return Container(
-      child: Text('$index'),
-      width: 100,
-      height: 52,
-      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-      alignment: Alignment.centerLeft,
     );
   }
 }
